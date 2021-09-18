4746619574f2f483d12647382c7286658ecc2a60
Velmurugan <velsans@gmail.com>
2021-09-18 14:19:13 +0530

    clear the codes

--- app/src/main/java/com/vel/openweatherapp/OpenWeatherFragment.kt
+++ app/src/main/java/com/vel/openweatherapp/OpenWeatherFragment.kt
@@ -1,23 +1,12 @@
 package com.vel.openweatherapp
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
-import android.widget.Toast
-import androidx.annotation.LayoutRes
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.vel.openweatherapp.SharedPrefsManager.get
-import com.vel.openweatherapp.SharedPrefsManager.set
-import com.vel.openweatherapp.databinding.DevbyteItemBinding
-import com.vel.openweatherapp.databinding.FragmentDevByteBinding
-import com.vel.openweatherapp.workmanager.WorkManagerScheduler
 import kotlinx.android.synthetic.main.weather_details.*
 import timber.log.Timber
 
